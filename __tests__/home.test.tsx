f4cbfb2ddef5929d17f7a5773a367feb8553edea
IshmamF <ishmamf2003@gmail.com>
2025-02-03 11:15:46 -0500

    configured jest, and wrote base test for home page

---
+++ __tests__/home.test.tsx
@@ -0,0 +1,10 @@
+//import '@testing-library/jest-dom'
+import {describe, it} from '@jest/globals';
+import { render } from '@testing-library/react'
+import Home from '../src/app/page'
+ 
+describe('Home Page', () => {
+  it('renders home page without crashing', () => {
+    render(<Home />)
+  })
+})
