abb6aeedebf3dc0635b3e636c6fb075fa501efc6
IshmamF <ishmamf2003@gmail.com>
2025-02-10 22:52:34 -0500

    created userInfoForm component for transport/address information

---
+++ src/app/(auth)/signup/_components/userInfoForm.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+interface SignupFormProps {
+  formData: Record<string, string>;
+  handleInputChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  toggleForm: (event: React.FormEvent<HTMLFormElement>) => void;
+}
+
+export const UserInfoForm: React.FC<SignupFormProps> = ({
+  formData,
+  handleInputChange,
+  toggleForm,
+}) => {
+  return (
+    <div>
+      <form
+        onSubmit={toggleForm}
+        className="flex flex-col w-[300px] md:w-[400px] text-jetBlack"
+      >
+        <label className="text-2xl font-bold mb-2" htmlFor="name">
+          Address
+        </label>
+        <input
+          className="border-2 border-jetBlack bg-lightBlue p-2 pl-4 rounded-lg mb-5"
+          id="address"
+          name="address"
+          type="text"
+          placeholder="Enter your address"
+          value={formData.address}
+          onChange={handleInputChange}
+          required
+        />
+        <label className="text-2xl font-bold mb-2" htmlFor="email">
+          Transport
+        </label>
+        <input
+          className="border-2 border-jetBlack bg-lightBlue p-2 pl-4 rounded-lg mb-5"
+          id="transport"
+          name="transport"
+          type="text"
+          placeholder="Enter your preferred mode of transport"
+          value={formData.transport}
+          onChange={handleInputChange}
+          required
+        />
+        <div className="flex gap-2 items-center justify-center w-full mt-3">
+            <div className="w-3 h-3 rounded-full bg-slate-400"></div>
+            <div className="w-3 h-3 rounded-full bg-black"></div>
+        </div>
+        <div className="flex gap-4">
+            <button name='previousButton' value='Toggles form' className="w-full mt-3 h-14 flex justify-center items-center rounded-md bg-darkBlue text-lightBlue hover:text-jetBlack hover:bg-gold transition-colors duration-300">
+                <p className="font-bold text-base md:text-xl">Previous</p>
+            </button>
+            <button name='submitButton' value='Submits form' className="w-full mt-3 h-14 flex justify-center items-center rounded-md bg-darkBlue text-lightBlue hover:text-jetBlack hover:bg-gold transition-colors duration-300">
+                <p className="font-bold text-base md:text-xl">Submit</p>
+            </button>
+        </div>
+      </form>
+    </div>
+  );
+};
