69af5894ee0562ec6159881fc5a45811a1764d42
IshmamF <ishmamf2003@gmail.com>
2025-02-13 22:42:28 -0500

    error page handling from nextjs

--- src/app/error/page.tsx
+++ src/app/error/page.tsx
@@ -1,7 +1,30 @@
-export default function ErrorPage() {
-    return (
-        <div>
-            Something went wrong...
-        </div>
-    )
+'use client' // Error components must be Client Components
+ 
+import { useEffect } from 'react'
+ 
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string }
+  reset: () => void
+}) {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error)
+  }, [error])
+ 
+  return (
+    <div>
+      <h2>Something went wrong!</h2>
+      <button
+        onClick={
+          // Attempt to recover by trying to re-render the segment
+          () => reset()
+        }
+      >
+        Try again
+      </button>
+    </div>
+  )
 }
