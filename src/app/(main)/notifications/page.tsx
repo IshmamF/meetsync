9f4be877876d0ab1dbccf95327e43944763a2869
IshmamF <ishmamf2003@gmail.com>
2025-02-15 21:27:37 -0500

    page structure

---
+++ src/app/(main)/notifications/page.tsx
@@ -0,0 +1,35 @@
+
+export default function Notification() {
+
+    return (
+        <div>
+            <div>Title</div>
+            <div>
+                <div>
+                    <div>
+                        Title: Last 7 Days
+                    </div>
+                    <div>
+                        List of Notifications
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        Title: Last 30 Days
+                    </div>
+                    <div>
+                        List of Notifications
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        Title: Older
+                    </div>
+                    <div>
+                        List of Notifications
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
