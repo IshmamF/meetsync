ad5038f768bb69888f1996f73e219194dee2f380
IshmamF <ishmamf2003@gmail.com>
2025-02-18 04:36:03 -0500

    sometimes providers don't give a new access token when u signin so added a fall back from the docs

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -37,7 +37,10 @@ export const authOptions: NextAuthOptions = {
             if (!AccountInfo) redirect('/login');
             if (account) {
                 // First-time login, save the `access_token`, its expiry and the `refresh_token`
-                const { data, error } = await supabase.schema('next_auth').from('accounts').upsert({
+                const { data, error } = await supabase
+                            .schema('next_auth')
+                            .from('accounts')
+                            .upsert({
                     user_id: AccountInfo.id,
                     email: user.email,
                     profile_img: user.image,
@@ -54,8 +57,68 @@ export const authOptions: NextAuthOptions = {
                     expires_at: account.expires_at || token.expires_at || Math.floor(Date.now() / 1000 + 86400),
                     refresh_token: account.refresh_token || token.refresh_token || "",
                 };
+            } else if (token.expires_at && Date.now() < token.expires_at * 1000) {
+                // Subsequent logins, but the `access_token` is still valid
+                return token
+            } else {
+                // Subsequent logins, but the `access_token` has expired, try to refresh it
+                if (!token.refresh_token) throw new TypeError("Missing refresh_token")
+         
+                try {
+                  // The `token_endpoint` can be found in the provider's documentation. Or if they support OIDC,
+                  // at their `/.well-known/openid-configuration` endpoint.
+                  // i.e. https://accounts.google.com/.well-known/openid-configuration
+                  const response = await fetch("https://oauth2.googleapis.com/token", {
+                    method: "POST",
+                    body: new URLSearchParams({
+                      client_id: GOOGLE_CLIENT!,
+                      client_secret: GOOGLE_SECRET!,
+                      grant_type: "refresh_token",
+                      refresh_token: token.refresh_token!,
+                    }),
+                  })
+         
+                  const tokensOrError = await response.json()
+         
+                  if (!response.ok) throw tokensOrError
+         
+                  const newTokens = tokensOrError as {
+                    access_token: string
+                    expires_in: number
+                    refresh_token?: string
+                  }
+
+                  const { data, error } = await supabase
+                  .schema('next_auth')
+                  .from('accounts')
+                  .upsert({
+                        user_id: AccountInfo.id,
+                        email: user.email,
+                        profile_img: user.image,
+                        access_token: newTokens.access_token!,
+                        expires_at: Math.floor(Date.now() / 1000 + newTokens.expires_in),
+                        refresh_token: newTokens.refresh_token!,
+                    });
+                    if (error) throw error
+         
+                  return {
+                    ...token,
+                    email: user.email ,
+                    profile_img: user.image,
+                    access_token: newTokens.access_token,
+                    expires_at: Math.floor(Date.now() / 1000 + newTokens.expires_in),
+                    // Some providers only issue refresh tokens once, so preserve if we did not get a new one
+                    refresh_token: newTokens.refresh_token
+                      ? newTokens.refresh_token
+                      : token.refresh_token,
+                  }
+                } catch (error) {
+                  console.error("Error refreshing access_token", error)
+                  // If we fail to refresh the token, return an error so we can handle it on the page
+                  token.error = "RefreshTokenError"
+                  return token
+                }
             }
-            return token
         },
         async session({ session, token }) {
             const supabase = await createClient();
@@ -104,6 +167,7 @@ export const authOptions: NextAuthOptions = {
                 } catch (error) {
                   console.error("Error refreshing access_token", error);
                   // If we fail to refresh the token, return an error so we can handle it on the page
+                  session.error = token.error
                 }
               }
               return session;   
@@ -111,3 +175,19 @@ export const authOptions: NextAuthOptions = {
     },
 };
 
+declare module "next-auth" {
+    interface Session {
+      error?: "RefreshTokenError"
+    }
+}
+  
+
+declare module "next-auth/jwt" {
+    interface JWT {
+      access_token: string
+      expires_at: number
+      refresh_token?: string
+      error?: "RefreshTokenError"
+    }
+}
+  
