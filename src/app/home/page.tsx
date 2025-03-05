4dd9e5050caab107ebc1df40cf7d2820dd7f799e
olukukoyi <oluwanifesimi.kukoyi15@gmail.com>
2025-03-05 17:41:36 -0500

    unfinished

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -9,15 +9,21 @@ function Home() {
     "John Doe",
     "Olu Kukoyi",
     "Mr Krabs",
+    "Mr Krabs",
     "Mr Krabs loves money",
-    "Mr Kfit-contentfit-contentfit-contentfit-content",
+    "Mr Krabs loves money",
+    "Mr Krabs loves money",
+    "Mr Krabs loves money",
+    "Mr Krabs loves money",
+    "Oluwanifesimi Kukoyi",
+    // "Mr Kfit-contentfit-contentfit-contentfit-content",
   ]);
 
   return (
-    <div className="grid h-screen grid-rows-[300px_auto_1fr]">
+    <div className="grid h-screen grid-rows-[500px_auto_1fr]">
       {/* First Row - Fixed Height */}
-      <div className="bg-blue-200 w-full h-[300px] flex flex-col items-start justify-center p-20">
-        <p className="text-white text-4xl pb-5 text-darkBlue">Start Hangout</p>
+      <div className="bg-blue-200 w-full h-[500px] flex flex-col items-start justify-center p-20">
+        <p className="text-4xl pb-5 text-darkBlue">Start Hangout</p>
         <div className="flex w-full h-full">
           <div>
             <p className="text-3xl text-darkBlue">Title</p>
@@ -27,7 +33,7 @@ function Home() {
             <p className="text-3xl">Attendees</p>
             <UserSearchBar users={attendees} />
           </div>
-          <div className="flex items-center justify-center ">
+          <div className="flex items-start justify-center mt-10">
             <button className="border-2 border-darkBlue text-darkBlue bg-gold w-[135px] h-[50px] rounded-2xl ml-10 ">
               Start Hangout
             </button>
--- src/components/home/UserSearchBar.tsx
+++ src/components/home/UserSearchBar.tsx
@@ -1,17 +1,53 @@
+import { supabase } from "@/utils/supabase/client";
 import { Search, CircleX } from "lucide-react";
-import React from "react";
+import React, { ChangeEvent, useState } from "react";
 
 type UserSearchBarProps = {
   users: string[];
 };
 
 function UserSearchBar({ users }: UserSearchBarProps) {
+  const [query, setQuery] = useState<string>();
+  const [results, setResults] = useState<Array<object>>();
+
+  async function seachUsers(e: ChangeEvent<HTMLInputElement>) {
+    setQuery(e.target.value);
+
+    const { data, error } = await supabase
+      .from("users")
+      .select("username")
+      .ilike("username", `%${query}%`);
+
+    if (error) {
+      console.error(error);
+    } else {
+      setResults(data);
+    }
+  }
+  console.log(results);
+
   return (
-    <div className="flex items-center justify-start w-full bg-white placeholder:text-slate-400 text-black text-lg border border-slate-200 rounded-md px-3 py-3 transition duration-300 ease w-[1100px]">
-      <Search />
-      {/* <div className="flex wrap gap-2 max-w-[700px] h-[1000px] bg-color-red-200 w-auto"> */}
-      <div className="pl-5 flex flex-wrap w-[800px] gap-3">
-        {/* <div className="pl-5 flex flex-wrap w-[800px] gap-3"> */}
+    <div className="flex flex-col items-center justify-start w-full bg-white placeholder:text-slate-400 text-black text-lg border border-slate-200 rounded-md px-3 py-3 transition duration-300 ease w-[1100px]">
+      <div className="pl-4 pr-6 flex items-center justify-center w-full mb-4">
+        <Search />
+        <div className="flex w-full flex-col">
+          <input
+            value={query}
+            onChange={seachUsers}
+            className=" bg-red-400 focus:outline-none h-[40px] pl-2 w-full"
+            placeholder="Enter your friend's name here..."
+          />
+          <div className="w-full flex flex-col ">
+            {results?.map((item) => (
+              <div className="flex flex-col w-full border-b-2  ">
+                <p className="ml-2 mt-3">hello</p>
+                <p className="ml-2 text-sm opacity-50">world</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-wrap w-[900px] gap-3 pl-4">
         {users.map((user) => {
           return (
             <div
@@ -24,12 +60,6 @@ function UserSearchBar({ users }: UserSearchBarProps) {
             </div>
           );
         })}
-      </div>
-      <div className="">
-        <input
-          className="bg-red-500 focus:outline-none "
-          placeholder="Enter your friend's name here..."
-        />
       </div>
     </div>
   );
