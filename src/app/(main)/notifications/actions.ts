efaab0e7fecfb867d9a2f4ca38bdde9d158269bb
IshmamF <ishmamf2003@gmail.com>
2025-03-05 09:21:35 -0500

    added updating and deleting notification functions

--- src/app/(main)/notifications/actions.ts
+++ src/app/(main)/notifications/actions.ts
@@ -1,32 +1,80 @@
 "use server"
 import { createClient } from "@/utils/supabase/server";
 
+const API_URL = "http://127.0.0.1:8000"
+
 export const fetchNotifications = async (): Promise<notif_response> => {
     const supabase = await createClient();
     const {data, error} = await supabase.auth.getUser();
 
-    /* Uncomment this when merging to main
-    if (error || !data?.user) {
+    if (error || !data) {
         console.error("Error fetching user:", error);
-        return {status: 500, notifications: []};
-    } */
+        return Promise.resolve({status: 500, notifications: []});
+    }
 
     const userId = data.user?.id;
 
     try {
-        const response = await fetch("http://127.0.0.1:8000/get-notifications", {
+        const response = await fetch(API_URL + "/get-notifications", {
             method: 'POST',
             headers: {
               'Content-Type': 'application/json'
             },
-            body: JSON.stringify({user_id: "1697a418-f5b2-4fc4-9ba6-124f6a332c0a"})
+            body: JSON.stringify({user_id: userId})
           });
         return response.json();
     } catch (err) {
         console.error("Error fetching notifications:", err);
-        return {status: 500, notifications: []};
+        return Promise.resolve({status: 500, notifications: []});
     }
+}
 
+export const deleteNotification = async (notification_id: number) => {
+    const supabase = await createClient();
+    const {data, error} = await supabase.auth.getUser();
+
+    if (error || !data) {
+        console.error("Error fetching user:", error);
+        return Promise.resolve({status: 500, message: "Something went wrong with fetching user"});
+    }
+    const userId = data.user?.id;
+
+    try {
+        const response = await fetch(API_URL + "/delete-notification", {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({user_id: userId, notification_id: notification_id.toString()})
+          });
+        return response.json();
+    } catch (err) {
+        console.error("Error deleting notifications:", err);
+        return Promise.resolve({status: 500, message: "Something went wrong with deleting notification" });
+    }
+}
+
+export const updateNotification = async (msg: string) => {
+    const supabase = await createClient();
+    const {data, error} = await supabase.auth.getUser();
+
+    if (error || !data) {
+        console.error("Error fetching user:", error);
+        return Promise.resolve({status: 500, message: "Something went wrong with fetching user"});
+    }
+    const userId = data.user?.id;
+
+    try {
+        const { error } = await supabase.from('notifications').update({message: msg, type: 'general'}).eq('user_id', userId)
+        if (error) {
+            console.error("Error updating notification:", error);
+            return Promise.resolve({status: 500, message: "Something went wrong with updating notification"});
+        }
+        return Promise.resolve({ status: 200, message: "Notification updated successfully" });
+    } catch (err) {
+        console.error("Error updating notifications:", err);
+        return Promise.resolve({status: 500, message: "Something went wrong with updating notification"});
+    }
 }
 
 type notif_response = {
