e1f3734a8e1708c61a5b11b6083f4a360b8c7a98
IshmamF <ishmamf2003@gmail.com>
2025-03-05 19:45:54 -0500

    interface for selecting availability, all of the frontend is done and can be readily used to send to backend

--- src/app/(private)/availability/page.tsx
+++ src/app/(private)/availability/page.tsx
@@ -1,34 +1,64 @@
+'use client'
+
+import {useState} from 'react'
+import QuickSelect from './_components/quickSelect';
+import SelectDays from './_components/selectDays';
+import SelectTimes from './_components/selectTimes';
+
 export default function AvailabilityPage() {
+    const [selectedDays, setSelectedDays] = useState<string[]>([]);
+    const [startTime, setStartTime] = useState("");
+    const [endTime, setEndTime] = useState("");
+    const [quickSelect, setQuickSelect] = useState("");
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        /* To Do 
+        - send information to backend
+        - fix schema for availability
+        */
+
+    }
     return (
         <div className="pt-8 bg-lightBlue min-h-screen text-black w-full px-6">
             <h1 className="font-extrabold text-3xl text-darkBlue pb-4">Submit Your Availability</h1>
-            <div className='bg-white w-full pb-6 pt-4 flex flex-col shadow-md rounded-lg'>
-                <div className="grid grid-cols-2">
-                    <div className="pl-5 ">
-                        <h2 className="font-bold text-2xl text-darkBlue pb-2">Select Days</h2>
-                        <div>
-                            multiple choice option for days of the week
-                        </div>
+            <form onSubmit={handleSubmit} className='bg-white w-full pb-6 pt-4 flex flex-col shadow-md rounded-lg'>
+                <div className="flex flex-col sm:flex-row">
+                    <div className="px-8 flex-1 sm:border-r sm:pr-8">
+                        <SelectDays
+                            selectedDays={selectedDays}
+                            setSelectedDays={setSelectedDays}
+                        />
                     </div>
-                    <div className="">
+                    <div className="flex-1 px-8 pt-6 sm:pt-0">
                         <div>
-                            <h2 className="font-bold text-2xl text-darkBlue pb-2">Select Times</h2>
-                            <div>
-                                start and end time inputs
-                            </div>
+                            <SelectTimes 
+                                startTime={startTime}
+                                endTime={endTime}
+                                setStartTime={setStartTime}
+                                setEndTime={setEndTime}
+                            />
                         </div>
                         <div className="pt-6">
-                            <h2 className="font-bold text-xl text-darkBlue/70 pb-2">Quick Select</h2>
-                            <div>
-                                single choice option for time range    
-                            </div>                        
+                            <QuickSelect
+                                quickSelect={quickSelect}
+                                setQuickSelect={setQuickSelect}
+                            />                         
                         </div>
                     </div>
                 </div>
-                <button className="mt-8 py-3 px-8 rounded-2xl bg-darkBlue text-white font-semibold shadow-md hover:bg-darkBlue/80 focus:outline-none focus:ring-2 focus:ring-darkBlue focus:ring-opacity-50 w-auto mx-auto">
+                <button 
+                        className={`mt-8 py-3 px-8 rounded-2xl text-white font-semibold shadow-md 
+                            ${( !(quickSelect || (startTime && endTime)) || (selectedDays.length == 0))
+                                ? 'bg-darkBlue/45 cursor-not-allowed' 
+                                : 'bg-darkBlue hover:bg-darkBlue/80 focus:outline-none focus:ring-2 focus:ring-darkBlue focus:ring-opacity-50'
+                            } 
+                            w-auto mx-auto`}
+                    disabled={( !(quickSelect || (startTime && endTime)) || (selectedDays.length == 0))}
+                >
                     Confirm Availability
                 </button>
-            </div>
+            </form>
         </div>
     );
 }
