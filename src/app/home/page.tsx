99554b777c5228e2870cbbedc4bc47f929d37cfd
olukukoyi <oluwanifesimi.kukoyi15@gmail.com>
2025-03-06 22:52:35 -0500

    cache

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,39 +1,53 @@
 "use client";
 
-import Input from "@/components/common/Input";
 import UserSearchBar from "@/components/home/UserSearchBar";
-import React, { useState } from "react";
+import React, { ChangeEvent, useState } from "react";
+
+export type Attendee = {
+  auth_id: string | null;
+  email: string | null;
+  username: string | null;
+};
 
 function Home() {
-  const [attendees, setAttendees] = useState<Array<string>>([
-    "John Doe",
-    "Olu Kukoyi",
-    "Mr Krabs",
-    "Mr Krabs",
-    "Mr Krabs loves money",
-    "Mr Krabs loves money",
-    "Mr Krabs loves money",
-    "Mr Krabs loves money",
-    "Mr Krabs loves money",
-    "Oluwanifesimi Kukoyi",
-    // "Mr Kfit-contentfit-contentfit-contentfit-content",
-  ]);
+  const [attendees, setAttendees] = useState<Attendee[]>([]);
+  const [title, setTitle] = useState<string>("");
+
+  function addAttendee(newAttendee: Attendee) {
+    setAttendees((prevAttendees) => [...prevAttendees, newAttendee]);
+  }
+
+  function removeAttendee(attendeeId: string) {
+    setAttendees((prevAttendees) =>
+      prevAttendees.filter((attendee) => attendee.auth_id != attendeeId)
+    );
+  }
 
   return (
     <div className="grid h-screen grid-rows-[500px_auto_1fr]">
-      {/* First Row - Fixed Height */}
       <div className="bg-blue-200 w-full h-[500px] flex flex-col items-start justify-center p-20">
         <p className="text-4xl pb-5 text-darkBlue">Start Hangout</p>
-        <div className="flex w-full h-full">
-          <div>
-            <p className="text-3xl text-darkBlue">Title</p>
-            <Input placeholder="Type Here..." />
+        <div className="flex w-full h-full mt-10">
+          <div className="w-[15%]">
+            <p className="text-5xl text-darkBlue">Title</p>
+            <input
+              value={title}
+              onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                setTitle(e.target.value);
+              }}
+              className="w-full mt-4 placeholder:text-slate-400 text-black text-lg border border-slate-200 rounded-md px-3 py-[10px] transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
+              placeholder="Type Here..."
+            />
           </div>
-          <div className="pl-10 text-darkBlue">
-            <p className="text-3xl">Attendees</p>
-            <UserSearchBar users={attendees} />
+          <div className="w-[30%]  pl-10 text-darkBlue">
+            <p className="text-5xl">Attendees</p>
+            <UserSearchBar
+              attendees={attendees}
+              addAttendee={addAttendee}
+              removeAttendee={removeAttendee}
+            />
           </div>
-          <div className="flex items-start justify-center mt-10">
+          <div className="flex items-start justify-center mt-[60px]">
             <button className="border-2 border-darkBlue text-darkBlue bg-gold w-[135px] h-[50px] rounded-2xl ml-10 ">
               Start Hangout
             </button>
@@ -41,12 +55,10 @@ function Home() {
         </div>
       </div>
 
-      {/* Second Row - Takes Up As Much Height As Possible */}
       <div className=" flex items-center justify-center">
         <p className="">Middle Section (Auto Height)</p>
       </div>
 
-      {/* Third Row - Remaining Height */}
       <div className="flex items-center justify-center">
         <p className="">Footer (Remaining Height)</p>
       </div>
--- src/components/home/UserSearchBar.tsx
+++ src/components/home/UserSearchBar.tsx
@@ -1,62 +1,116 @@
+"use client";
+
+import { Attendee } from "@/app/home/page";
 import { supabase } from "@/utils/supabase/client";
 import { Search, CircleX } from "lucide-react";
-import React, { ChangeEvent, useState } from "react";
+import React, { ChangeEvent, useEffect, useRef, useState } from "react";
 
 type UserSearchBarProps = {
-  users: string[];
+  attendees: Attendee[];
+  addAttendee(newAttendee: Attendee): void;
+  removeAttendee(attendeeId: string): void;
 };
 
-function UserSearchBar({ users }: UserSearchBarProps) {
-  const [query, setQuery] = useState<string>();
-  const [results, setResults] = useState<Array<object>>();
+function UserSearchBar({
+  attendees,
+  addAttendee,
+  removeAttendee,
+}: UserSearchBarProps) {
+  const [query, setQuery] = useState<string>("");
+  const [results, setResults] = useState<Attendee[]>([]);
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const autocompleteRef = useRef<HTMLDivElement>(null); // Reference for click detection
 
   async function seachUsers(e: ChangeEvent<HTMLInputElement>) {
     setQuery(e.target.value);
 
-    const { data, error } = await supabase
-      .from("users")
-      .select("username")
-      .ilike("username", `%${query}%`);
-
-    if (error) {
-      console.error(error);
-    } else {
-      setResults(data);
+    if (query === "") {
+      setResults([]);
+      return;
     }
+
+    const results = await fetch(
+      `http://0.0.0.0:8000/friends-autocomplete/?query=${query}&uuid=${uuid}`
+    );
   }
-  console.log(results);
+
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (
+        autocompleteRef.current &&
+        !autocompleteRef.current.contains(event.target as Node)
+      ) {
+        setIsOpen(false);
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
   return (
-    <div className="flex flex-col items-center justify-start w-full bg-white placeholder:text-slate-400 text-black text-lg border border-slate-200 rounded-md px-3 py-3 transition duration-300 ease w-[1100px]">
-      <div className="pl-4 pr-6 flex items-center justify-center w-full mb-4">
+    <div
+      onClick={() => {
+        if (results.length >= 1) {
+          setIsOpen(true);
+        }
+      }}
+      ref={autocompleteRef}
+      className="flex mt-4 flex-col items-center justify-start w-full bg-white placeholder:text-slate-400 text-black text-lg border border-slate-200 rounded-md px-3 py-1 transition duration-300 ease w-[1100px]"
+    >
+      <div className="relative  pl-4 pr-6 flex items-center justify-center w-full ">
         <Search />
-        <div className="flex w-full flex-col">
+        <div className="relative flex w-full flex-col">
           <input
             value={query}
             onChange={seachUsers}
-            className=" bg-red-400 focus:outline-none h-[40px] pl-2 w-full"
+            className="focus:outline-none h-[40px] pl-2 w-full"
             placeholder="Enter your friend's name here..."
           />
-          <div className="w-full flex flex-col ">
-            {results?.map((item) => (
-              <div className="flex flex-col w-full border-b-2  ">
-                <p className="ml-2 mt-3">hello</p>
-                <p className="ml-2 text-sm opacity-50">world</p>
-              </div>
-            ))}
-          </div>
+          {/* suggestions box */}
+          {isOpen && (
+            <div className="absolute top-[34px] bg-gray-200   w-full flex flex-col">
+              {results?.map((item: Attendee) => (
+                <div
+                  key={item.auth_id}
+                  onClick={() => {
+                    if (item && item.username) {
+                      addAttendee(item);
+                      setIsOpen(false);
+                      setResults([]);
+                      setQuery("");
+                    }
+                  }}
+                  className="flex flex-col w-full border-b-2  "
+                >
+                  <p className="ml-2 mt-3">{item.username}</p>
+                  <p className="ml-2 mb-2 text-sm opacity-50">{item.email}</p>
+                </div>
+              ))}
+            </div>
+          )}
         </div>
       </div>
-      <div className="flex flex-wrap w-[900px] gap-3 pl-4">
-        {users.map((user) => {
+      <div className="flex flex-wrap w-full gap-3 pl-10 mt-2">
+        {attendees.map((user) => {
           return (
             <div
-              key={user}
+              key={user.auth_id}
               className="flex items-center justify-start border-2 text-darkBlue border-gold rounded-2xl p-2 h-auto inline-block pr-3"
             >
-              {user}
+              {user.username}
 
-              <CircleX className="pl-2" color="red" />
+              <CircleX
+                onClick={() => {
+                  if (user && user.auth_id) {
+                    removeAttendee(user.auth_id);
+                  }
+                }}
+                className="pl-2"
+                color="red"
+              />
             </div>
           );
         })}
