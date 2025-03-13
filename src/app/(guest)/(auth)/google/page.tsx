24b8580d0e997ceab9a65b50bf43c66863d9d9ac
Abdul-Andha <aandha8342@gmail.com>
2025-03-13 12:55:46 -0400

    address autofill

--- src/app/(guest)/(auth)/google/page.tsx
+++ src/app/(guest)/(auth)/google/page.tsx
@@ -1,13 +1,13 @@
-'use client';
+"use client";
 
-import { UserInfoForm } from './components/userInfoForm';
-import {SaveUserPrefData} from './actions';
+import { UserInfoForm } from "./components/userInfoForm";
+import { SaveUserPrefData } from "./actions";
 import { signIn } from "next-auth/react";
-import { useState } from 'react';
+import { useState } from "react";
 
 type UserFormData = {
-  address: string,
-  transport: string,
+  address: string;
+  transport: string;
 };
 
 export default function CalendarPage() {
@@ -16,8 +16,10 @@ export default function CalendarPage() {
     address: "",
     transport: "",
   });
-  
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+
+  const handleInputChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
     const { name, value } = e.target;
     setFormData((prevData) => ({
       ...prevData,
@@ -26,31 +28,31 @@ export default function CalendarPage() {
   };
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-      e.preventDefault();
+    e.preventDefault();
 
-      if (loading) return;
+    if (loading) return;
 
-      setLoading(true);
+    setLoading(true);
 
-      const response = await SaveUserPrefData(formData);
-      if (response.status != 200) {
-        console.error(response.error);
-        setLoading(false);
-        return;
-      }
+    const response = await SaveUserPrefData(formData);
+    if (response.status != 200) {
+      console.error(response.error);
+      setLoading(false);
+      return;
+    }
 
-      await signIn("google", { callbackUrl: '/' });
-
+    await signIn("google", { callbackUrl: "/home" });
   };
 
   return (
     <div className="bg-lightBlue min-h-screen flex flex-col gap-6 justify-center items-center">
       <div className="text-5xl font-bold text-jetBlack pb-4">Information</div>
       <UserInfoForm
-      formData={formData}
-      handleInputChange={handleInputChange}
-      handleSubmit={handleSubmit}
-      loading={loading}/>
+        formData={formData}
+        handleInputChange={handleInputChange}
+        handleSubmit={handleSubmit}
+        loading={loading}
+      />
     </div>
-  )
+  );
 }
