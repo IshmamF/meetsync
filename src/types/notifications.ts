16005572d1a386fb138072d9529646a5654ec64c
IshmamF <ishmamf2003@gmail.com>
2025-03-20 12:37:25 -0400

    added notificationTypes and updated notification type

--- src/types/notifications.ts
+++ src/types/notifications.ts
@@ -1,9 +1,24 @@
-export type notification = {
+export interface notification {
     id: number,
     user_id: string,
     message: string, 
     is_read: boolean, 
     created_at: string, 
     sender: string | null, 
-    type: string 
+    type: NotificationType,
+    users: userProfile,
+    hangout_id: string | null
 } 
+
+export enum NotificationType {
+    HANGOUT_INVITE = 'hangout-invite',
+    FRIEND_REQUEST = 'friend-request',
+    GENERAL = 'general',
+    SELECT_AVAILABILITY = 'select-availability'
+}
+
+
+type userProfile = {
+    username: string,
+    profile_img: string | null
+}
