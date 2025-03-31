29e1c6322c52385a0a9d914be90c51ebe765d2e6
IshmamF <ishmamf2003@gmail.com>
2025-03-31 07:34:27 -0400

    useEffect to get the meetup options and pass into voter component

--- src/app/(private)/availability/[id]/_components/availability.tsx
+++ src/app/(private)/availability/[id]/_components/availability.tsx
@@ -1,13 +1,19 @@
 'use client'
 import { useUser } from '@/utils/context/userContext';
 import { useEffect, useState } from 'react';
-import { getHangoutInfo } from '../actions';
+import { getHangoutInfo, getMeetupOptions } from '../actions';
 import CreatorPage from './creatorPage';
 import VoterPage from './voterPage';
+import toast from 'react-hot-toast';
 
 
 interface Props {
     id: string
+}
+
+type MeetupOption = {
+    id: string,
+    option: string
 }
 
 export default function Availability({id}: Props) {
@@ -15,19 +21,41 @@ export default function Availability({id}: Props) {
     const [isCreator, setIsCreator] = useState<boolean>(false);
     const [displayTitle, setDisplayTitle] = useState<string>('')
     const [isLoading, setIsLoading] = useState<boolean>(true);
-
+    const [options, setOptions] = useState<MeetupOption[]>([]);
     
     useEffect(() => {
         const hangoutInfo = async () => {
-            const info = await getHangoutInfo(id);
-            if (info.hangout_info?.creator_id == user?.auth_id) {
-                setIsCreator(true);
+            try {
+                const info = await getHangoutInfo(id);
+                if (info.hangout_info?.creator_id == user?.auth_id) {
+                    setIsCreator(true);
+                }
+                setDisplayTitle(info.hangout_info?.title!);
+                setIsLoading(false);
+            } catch(error){
+                toast.error('An error occurred while loading hangout info. Please try refreshing.');
+                console.error('Error loading hangout info:', error);
+                setIsLoading(false);
             }
-            setDisplayTitle(info.hangout_info?.title!);
-            setIsLoading(false);
         }
         hangoutInfo();
     }, [])
+
+    useEffect(() => {
+        setIsLoading(true);
+        const loadMeetupOptions = async () => {
+            try {
+                const info = await getMeetupOptions(id);
+                setOptions(info?.options);
+                setIsLoading(false);
+            } catch(error) {
+                toast.error('An error occurred while loading options. Please try refreshing.');
+                console.error('Error loading meetup options:', error);
+                setIsLoading(false);
+            }
+        }
+        loadMeetupOptions();
+    }, [isCreator, displayTitle])
 
     if (isLoading) {
         return (
@@ -37,7 +65,7 @@ export default function Availability({id}: Props) {
 
     return (
         <>
-            {isCreator ? (<CreatorPage title={displayTitle} hangout_id={id}/>) : (<VoterPage/>)}
+            {isCreator ? (<CreatorPage title={displayTitle} hangout_id={id}/>) : (<VoterPage options={options} title={displayTitle}/>)}
         </>
     )
 }
