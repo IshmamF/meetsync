3d78cb08de6bea8ffbd131eb6c938d83785b39a2
Abdul-Andha <aandha8342@gmail.com>
2025-04-11 21:08:15 -0400

    connect vote on location

--- src/app/(private)/hangouts/_components/HangoutActionButton.tsx
+++ src/app/(private)/hangouts/_components/HangoutActionButton.tsx
@@ -14,16 +14,13 @@ export default function HangoutActionButton({
   return (
     <div>
       {flowStatus === "pending-time-vote" ? (
-        <Link
-          href={`/availability/${id}`}
-          className="border-darkBlue border rounded-md px-3 py-1 cursor-pointer hover:bg-gold transition-all duration-300 ease-in-out"
-        >
-          Vote on Availability
-        </Link>
+        <div className="border-darkBlue border rounded-md px-3 py-1 cursor-pointer hover:bg-gold transition-all duration-300 ease-in-out">
+          <Link href={`/availability/${id}`}>Vote on Availability</Link>
+        </div>
       ) : flowStatus === "pending-location-vote" ? (
-        <button className="border-darkBlue border rounded-md px-3 py-1 cursor-pointer hover:bg-gold transition-all duration-300 ease-in-out">
-          Vote on Location
-        </button>
+        <div className="border-darkBlue border rounded-md px-3 py-1 cursor-pointer hover:bg-gold transition-all duration-300 ease-in-out">
+          <Link href={`/meeting-recommendations/${id}`}>Vote on Location</Link>
+        </div>
       ) : flowStatus === "pending-confirm-time" ? (
         <button className="border-darkBlue border rounded-md px-3 py-1 cursor-pointer hover:bg-gold transition-all duration-300 ease-in-out">
           Confirm Availability
