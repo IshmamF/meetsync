f201ec3bd1df7ea88ad1c1b2c4b8b41297a4649d
olukukoyi <oluwanifesimi.kukoyi15@gmail.com>
2025-05-04 21:25:52 -0400

    replaced svg with react component

--- src/app/(private)/hangouts/_components/AddFriendModal.tsx
+++ src/app/(private)/hangouts/_components/AddFriendModal.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { ChangeEvent, useRef, useState } from "react";
-import { XIcon } from "@/app/components/Icons";
+import { CheckIcon, XIcon } from "@/app/components/Icons";
 import { getApiBase } from "@/utils/etc/apiBase";
 import toast from "react-hot-toast";
 import { useUser } from "@/utils/context/userContext";
@@ -114,20 +114,7 @@ const AddFriendModal = ({
                     {requestSent.includes(person.auth_id) ||
                     pendingFriends.includes(person.auth_id) ? (
                       <div className="flex items-center justify-center w-[25%]">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="24"
-                          height="24"
-                          viewBox="0 0 24 24"
-                          fill="none"
-                          stroke="currentColor"
-                          strokeWidth="2"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          className="lucide lucide-check-icon lucide-check"
-                        >
-                          <path d="M20 6 9 17l-5-5" />
-                        </svg>
+                        <CheckIcon />
                       </div>
                     ) : (
                       <button
